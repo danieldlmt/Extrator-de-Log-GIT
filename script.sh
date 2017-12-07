cb964b37ce234a4e52ebadb1ac513e739a342e2f
danieldlmt <danieldlmt@gmail.com>
2017-12-07 14:42:17 -0300

    Adicionado filtro de no-merges e filtro de arquivos adicionados

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ revlist=$(git rev-list HEAD)
 (
     for rev in $revlist
     do
-            files=$(git log -1 --pretty="format:" --name-only --diff-filter=M $rev)
+            files=$(git log -1 --pretty="format:" --name-only --no-merges --diff-filter=MA $rev)
             for file in $files
             do
                     echo " $(git log -1 --pretty="%H;%ce;%ci" $rev);$file"
