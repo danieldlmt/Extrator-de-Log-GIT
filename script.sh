1427a9a53c9329461e0bced9991afc7311eee480
danieldlmt <danieldlmt@gmail.com>
2017-12-13 12:48:20 -0300

    Adição do parametro --numstat para pegar numero de linhas modificadas em cada arquivo

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 revlist=$(git rev-list HEAD)
+IFS=$'\n'
 (
     for rev in $revlist
     do
-            files=$(git log -1 --pretty="format:" --name-only --no-merges --diff-filter=MA $rev)
+            files=$(git log -1 --numstat --no-merges --diff-filter=MA $rev | grep "^[0-9]")
             for file in $files
             do
                     echo " $(git log -1 --pretty="%H;%ce;%ci" $rev);$file"
